173e4e3e1ec7fae11dce4ddc9d6eb5f2d2cd3c02
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2019-02-14 00:31:30 +0100

    up.sh: update debian rootfs
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

--- up.sh
+++ up.sh
@@ -44,7 +44,7 @@ wget https://cloud.3mdeb.com/index.php/s/UQQVYrNIhg7ddwj/download -O kernels.tar
 tar -xzvf kernels.tar.gz -C ./netboot && rm kernels.tar.gz
 
 # download root file systems
-wget https://cloud.3mdeb.com/index.php/s/9b8h6WmJcNsuB57/download -O debian-stable.tar.gz
+wget https://cloud.3mdeb.com/index.php/s/a7FJar3disZrEfs/download -O debian-stable.tar.gz
 wget https://cloud.3mdeb.com/index.php/s/fzQ2FaRTdMvzXqO/download -O xen.tar.gz
 wget https://cloud.3mdeb.com/index.php/s/AQuUdsYkBzO9UJz/download -O core.tar.gz
 wget https://cloud.3mdeb.com/index.php/s/rUZPwRHOjxpSxN4/download -O voyage-0.11.0_amd64.tar.gz
